9ffed76ebfee1ef13197af6b9a3c4844497811ad
Grach90 <grach.tver777@gmail.com>
2021-07-26 19:18:04 +0300

    first commit

--- server/app.js
+++ server/app.js
@@ -15,8 +15,9 @@ const dbConnection = mongoose.connection;
 dbConnection.on('error', error => console.log(`Error: ${error}`));
 dbConnection.once('open', () => console.log('Connected to DB!'));
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 app.use(cors());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use('/', router);
 
